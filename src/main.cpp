e9d21ec77ff6adfb1ecd0843bbc8ee928d8d9610
Roman Hosek <romanhosek.cz@gmail.com>
2023-12-08 20:21:51 +0100

    wifi improv

--- src/main.cpp
+++ src/main.cpp
@@ -321,6 +321,8 @@ GxEPD2_3C<GxEPD2_154c, GxEPD2_154c::HEIGHT / 2> display(GxEPD2_154c(PIN_SS, PIN_
 #include <ESP32AnalogRead.h>
 // Font
 #include "OpenSansSB_12px.h"
+// WifiSetup
+#include "improv.h"
 
 #ifdef REMAP_SPI
   SPIClass hspi(HSPI);
@@ -462,12 +464,15 @@ void displayInit()
   display.setTextColor(GxEPD_BLACK);  // black font
 }
 
-void WiFiInit()
+boolean WiFiInit(boolean improv,std::string ssid, std::string password)
 {
   // Connecting to WiFi
   Serial.println();
   Serial.print("Connecting... ");
-  //WiFi.begin('ssid', pass);
+  if(improv){
+    WiFi.begin(ssid.c_str(), password.c_str());
+  }
+  
   WiFi.mode(WIFI_STA);
   WiFiManager wm;
   wm.setWiFiAutoReconnect(true);
@@ -518,11 +523,13 @@ void WiFiInit()
 
   if(!res) {
       Serial.println("Failed to connect to Wi-Fi, starting portal");
+      return false;
   } 
-  else {
-        //if you get here you have connected to the WiFi    
-        Serial.println("Wi-Fi connected successfully");
-  }
+
+   //if you get here you have connected to the WiFi    
+   Serial.println("Wi-Fi connected successfully");
+   return true;
+
 }
 
 uint32_t skip(WiFiClient& client, int32_t bytes)
@@ -1236,7 +1243,7 @@ void setup()
   displayInit();
 
   // Wifi init
-  WiFiInit();
+  WiFiInit(false, "","");
 
   // WiFi strength - so you will know how good your signal is
   getWifiStrength();
@@ -1284,3 +1291,160 @@ void loop()
 {
 
 }
+
+// *** Improv
+
+std::vector<std::string> getLocalUrl() {
+  return {
+    // URL where user can finish onboarding or use device
+    // Recommended to use website hosted by device
+    String("http://" + WiFi.localIP().toString()).c_str()
+  };
+}
+
+void onErrorCallback(improv::Error err) {
+  Serial.print(err);
+}
+
+void send_response(std::vector<uint8_t> &response) {
+  std::vector<uint8_t> data = {'I', 'M', 'P', 'R', 'O', 'V'};
+  data.resize(9);
+  data[6] = improv::IMPROV_SERIAL_VERSION;
+  data[7] = improv::TYPE_RPC_RESPONSE;
+  data[8] = response.size();
+  data.insert(data.end(), response.begin(), response.end());
+
+  uint8_t checksum = 0x00;
+  for (uint8_t d : data)
+    checksum += d;
+  data.push_back(checksum);
+
+  Serial.write(data.data(), data.size());
+}
+
+void set_error(improv::Error error) {
+  std::vector<uint8_t> data = {'I', 'M', 'P', 'R', 'O', 'V'};
+  data.resize(11);
+  data[6] = improv::IMPROV_SERIAL_VERSION;
+  data[7] = improv::TYPE_ERROR_STATE;
+  data[8] = 1;
+  data[9] = error;
+
+  uint8_t checksum = 0x00;
+  for (uint8_t d : data)
+    checksum += d;
+  data[10] = checksum;
+
+  Serial.write(data.data(), data.size());
+}
+
+void set_state(improv::State state) {  
+  
+  std::vector<uint8_t> data = {'I', 'M', 'P', 'R', 'O', 'V'};
+  data.resize(11);
+  data[6] = improv::IMPROV_SERIAL_VERSION;
+  data[7] = improv::TYPE_CURRENT_STATE;
+  data[8] = 1;
+  data[9] = state;
+
+  uint8_t checksum = 0x00;
+  for (uint8_t d : data)
+    checksum += d;
+  data[10] = checksum;
+
+  Serial.write(data.data(), data.size());
+}
+
+void getAvailableWifiNetworks() {
+  int networkNum = WiFi.scanNetworks();
+
+  for (int id = 0; id < networkNum; ++id) { 
+    std::vector<uint8_t> data = improv::build_rpc_response(
+            improv::GET_WIFI_NETWORKS, {WiFi.SSID(id), String(WiFi.RSSI(id)), (WiFi.encryptionType(id) == WIFI_AUTH_OPEN ? "NO" : "YES")}, false);
+    send_response(data);
+    delay(1);
+  }
+  //final response
+  std::vector<uint8_t> data =
+          improv::build_rpc_response(improv::GET_WIFI_NETWORKS, std::vector<std::string>{}, false);
+  send_response(data);
+}
+
+bool onCommandCallback(improv::ImprovCommand cmd) {
+
+  switch (cmd.command) {
+    case improv::Command::GET_CURRENT_STATE:
+    {
+      if ((WiFi.status() == WL_CONNECTED)) {
+        set_state(improv::State::STATE_PROVISIONED);
+        std::vector<uint8_t> data = improv::build_rpc_response(improv::GET_CURRENT_STATE, getLocalUrl(), false);
+        send_response(data);
+
+      } else {
+        set_state(improv::State::STATE_AUTHORIZED);
+      }
+      
+      break;
+    }
+
+    case improv::Command::WIFI_SETTINGS:
+    {
+      if (cmd.ssid.length() == 0) {
+        set_error(improv::Error::ERROR_INVALID_RPC);
+        break;
+      }
+     
+      set_state(improv::STATE_PROVISIONING);
+      
+      
+      if (WiFiInit(true, cmd.ssid, cmd.password)) {
+
+        set_state(improv::STATE_PROVISIONED);        
+        std::vector<uint8_t> data = improv::build_rpc_response(improv::WIFI_SETTINGS, getLocalUrl(), false);
+        send_response(data);
+
+      } else {
+        set_state(improv::STATE_STOPPED);
+        set_error(improv::Error::ERROR_UNABLE_TO_CONNECT);
+      }
+      
+      break;
+    }
+
+    case improv::Command::GET_DEVICE_INFO:
+    {
+      std::vector<std::string> infos = {
+        // Firmware name
+        "ZivyObraz",
+        // Firmware version
+        "1.0.0",
+        // Hardware chip/variant
+        "ESP32",
+        // Device name
+        "ZivyObraz"
+      };
+      std::vector<uint8_t> data = improv::build_rpc_response(improv::GET_DEVICE_INFO, infos, false);
+      send_response(data);
+      break;
+    }
+
+    case improv::Command::GET_WIFI_NETWORKS:
+    {
+      getAvailableWifiNetworks();
+      break;
+    }
+
+    default: {
+      set_error(improv::ERROR_UNKNOWN_RPC);
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+
+
+
+
